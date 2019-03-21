ac228dc3d3160d0eb3589aca0705cd5318c9afb8
Aaron Renner <aaron.renner@gmail.com>
2019-03-21 14:59:43 -0600

    Add YAML support

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -33,7 +33,7 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(
+   '(yaml
      html
      javascript
      elixir
