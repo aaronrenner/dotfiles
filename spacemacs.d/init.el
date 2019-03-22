72081d4caeec56c52eca285e55a013e951071230
Aaron Renner <aaron.renner@gmail.com>
2019-03-21 21:20:25 -0600

    Disable fly-spell-auto-completion
    This was getting in the way because it moved my cursor to the end of a misspelled
    word when I wanted to edit it. It seems like transient mode (SPC S .) is going
    to work better.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -54,7 +54,7 @@ This function should only modify configuration layer settings."
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
-     (spell-checking :variables enable-flyspell-auto-completion t)
+     spell-checking
      ;; syntax-checking
      ;; version-control
      )
